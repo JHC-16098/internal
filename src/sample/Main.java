537f6d8c4d6221643684c8aadf96c0921964e7bd
JHC-16098 <16098@jhc.school.nz>
2021-04-14 10:49:39 +1200

    first

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -16,14 +16,14 @@ import javafx.scene.layout.GridPane;
 import org.apache.commons.lang3.ArrayUtils;
 
 public class Main extends Application {
-/*
+
     public static class encryption {
         //Char array which holds all characters supported by the cipher
         //private static char[] cipher = {'\t', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ', '.', '_', ',', ':', '"', '-', '\'', '/', '\\', '=', '+', '?' , '!', '$', '*', '>', '<', '@', '{', '}', '(', ')', '[', ']', ';', '\n', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
         private static char[] cipher = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' '};
         //Constant which holds the length of the char array
         static int ARRAY_LENGTH = cipher.length;
-
+/*
         // Method which replaces a character in a specific position in a string. Takes the position of
         // the character to replace, which character to replace it with, and the string which you want
         // to replace the character in
@@ -59,7 +59,7 @@ public class Main extends Application {
             System.out.println("Char at method: " + offsetChar);
             return offsetChar;
         }
-
+*/
         // Function which generates the character offset by multiplying the key by the first 9 digits of pi, then dividing
         // by 2 until it is less than the length of the cipher array. This is then set as the new offset.
         public static int getOffset(long key) {
@@ -78,19 +78,19 @@ public class Main extends Application {
             System.out.println("Final offset int: " + offsetInt);
             return offsetInt;
         }
-
+/*
         // Method to get an inverted offset for decrypting.
         public static int getDecryptOffset(int offset) {
             int decryptOffset = ((ARRAY_LENGTH + offset)*(0-1));
             return decryptOffset;
         }
-
+*/
         // Method which checks if an integer is positive or not
         static int checkPositive(int num) {
             int result = Integer.signum(num);
             return result;
         }
-
+/*
         // Method whick encrypts a string using an offset. Takes the source string and the key as arguments.
         public static String encrypt(String cleartext, long key) {
             int charOffset = encryption.getOffset(key); // Run method to get offset from key
@@ -127,9 +127,9 @@ public class Main extends Application {
             }
             System.out.println(encrypted);
             return encrypted;
-        }
+*/        //}
     }
-*/
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         // Create grid for GUI, st window title, and UI padding
