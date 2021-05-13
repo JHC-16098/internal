eeae73a938f2ba4453a3e8d1daaab8ed6af6f327
JHC-16098 <16098@jhc.school.nz>
2021-05-14 11:36:22 +1200

    first

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -18,12 +18,25 @@ import org.apache.commons.lang3.ArrayUtils;
 public class Main extends Application {
 
     public static class encryption {
+
+        public static void wait(int ms)
+        {
+            try
+            {
+                Thread.sleep(ms);
+            }
+            catch(InterruptedException ex)
+            {
+                Thread.currentThread().interrupt();
+            }
+        }
+
         //Char array which holds all characters supported by the cipher
-        //private static char[] cipher = {'\t', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ', '.', '_', ',', ':', '"', '-', '\'', '/', '\\', '=', '+', '?' , '!', '$', '*', '>', '<', '@', '{', '}', '(', ')', '[', ']', ';', '\n', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-        private static char[] cipher = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' '};
+        private static char[] cipher = {'\t', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ', '.', '_', ',', ':', '"', '-', '\'', '/', '\\', '=', '+', '?' , '!', '$', '*', '>', '<', '@', '#', '%', '^', '&', '*', '{', '}', '(', ')', '[', ']', ';', '\n', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        //private static char[] cipher = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' '};
         //Constant which holds the length of the char array
         static int ARRAY_LENGTH = cipher.length;
-/*
+
         // Method which replaces a character in a specific position in a string. Takes the position of
         // the character to replace, which character to replace it with, and the string which you want
         // to replace the character in
@@ -59,7 +72,7 @@ public class Main extends Application {
             System.out.println("Char at method: " + offsetChar);
             return offsetChar;
         }
-*/
+
         // Function which generates the character offset by multiplying the key by the first 9 digits of pi, then dividing
         // by 2 until it is less than the length of the cipher array. This is then set as the new offset.
         public static int getOffset(long key) {
@@ -78,22 +91,30 @@ public class Main extends Application {
             System.out.println("Final offset int: " + offsetInt);
             return offsetInt;
         }
-/*
+
         // Method to get an inverted offset for decrypting.
         public static int getDecryptOffset(int offset) {
             int decryptOffset = ((ARRAY_LENGTH + offset)*(0-1));
             return decryptOffset;
         }
-*/
+
         // Method which checks if an integer is positive or not
         static int checkPositive(int num) {
             int result = Integer.signum(num);
             return result;
         }
-/*
+
         // Method whick encrypts a string using an offset. Takes the source string and the key as arguments.
-        public static String encrypt(String cleartext, long key) {
-            int charOffset = encryption.getOffset(key); // Run method to get offset from key
+        public static void encrypt(String cleartext, long key, int mode, TextArea textBox) {
+            int charOffset = 0;
+
+            if (mode == 0) {
+                charOffset = encryption.getOffset(key);
+            }
+            else if (mode == 1) {
+                charOffset = (encryption.getDecryptOffset(encryption.getOffset(key)));
+            }
+            //int charOffset = encryption.getOffset(key); // Run method to get offset from key
 
             // Iterates over each character in the source string, using the encryptNewChar method to get the
             // offset char, then uses the replaceCharInPosition method to replace the old char with the offset
@@ -102,32 +123,16 @@ public class Main extends Application {
                 char[] tempCharArray = {cleartext.charAt(c)}; // Converts the individual character into a one-element char array to be passed to encryptNewChar
                 System.out.println("tempCharArray: " + tempCharArray[0]);
                 char charAtOffset = encryption.encryptNewChar(tempCharArray[0], charOffset);
-                    System.out.println("Offset char in function: " + charAtOffset + "\n--------------------------");
-                    cleartext = encryption.replaceCharInPosition(c, charAtOffset, cleartext);
+                System.out.println("Offset char in function: " + charAtOffset + "\n--------------------------");
+                cleartext = encryption.replaceCharInPosition(c, charAtOffset, cleartext);
+                textBox.setText(cleartext);
+
+
             }
 
             System.out.println(cleartext);
-            return cleartext;
         }
 
-        //Similar to encrypt, except it uses getDecryptOffset to generate an inverted offset
-        public static String decrypt(String encrypted, long key) {
-            // get decrypt offset by passing the result off the getOffset method to the decryptOffset function, which reverses it.
-            int charOffset = (encryption.getDecryptOffset(encryption.getOffset(key)));
-
-            // Same as encrypt - iterates over each character in the encrypted string, using the encryptNewChar method
-            // to get the offset char, then uses the replaceCharInPosition method to replace the old char with the offset
-            // char from encryptNewChar.
-            for (int c=0; c < encrypted.length(); c++) {
-                char[] tempCharArray = {encrypted.charAt(c)};
-                System.out.println("tempCharArray: " + tempCharArray[0]);
-                char charAtOffset = encryption.encryptNewChar(tempCharArray[0], charOffset);
-                System.out.println("Offset char in function: " + charAtOffset + "\n--------------------------");
-                encrypted = encryption.replaceCharInPosition(c, charAtOffset, encrypted);
-            }
-            System.out.println(encrypted);
-            return encrypted;
-*/        //}
     }
 
     @Override
@@ -174,15 +179,15 @@ public class Main extends Application {
         //Add HBox to grid
         grid.add(buttons, 0, 4);
 
-        /*
+
         //Event handler for Encrypt button which calls the encrypt method
         encryptButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e) {
                 String cleartext = message.getText();
                 long key = Integer.parseInt(keyField.getText());
-                String encryptedText = encryption.encrypt(cleartext, key);
-                message.setText(encryptedText);
+                encryption.encrypt(cleartext, key, 0, message);
+                //message.setText(encryptedText);
             }
         });
 
@@ -193,18 +198,18 @@ public class Main extends Application {
             public void handle(ActionEvent e) {
                 String cleartext = message.getText();
                 long key = Integer.parseInt(keyField.getText());
-                String encryptedText = encryption.decrypt(cleartext, key);
-                message.setText(encryptedText);
+                encryption.encrypt(cleartext, key, 1, message);
+                //message.setText(encryptedText);
             }
         });
 
-        */
+
 
         //Set window size, add scene to window
         Scene scene = new Scene(grid, 500, 350);
         primaryStage.setScene(scene);
         //Import CSS file
-        //scene.getStylesheets().add(Main.class.getResource("sample.css").toExternalForm());
+        scene.getStylesheets().add(Main.class.getResource("sample.css").toExternalForm());
         //Show stage on window
         primaryStage.show();
     }
